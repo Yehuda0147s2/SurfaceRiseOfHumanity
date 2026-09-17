2116e686eb0e353b62f2165321358e7f4eea3214
yehuda <34794543+Yehuda0147s2@users.noreply.github.com>
2026-09-17 16:55:46 +0300

    Add stable mission objective IDs and save restoration

--- Assets/Scripts/Missions/MissionObjective.cs
+++ Assets/Scripts/Missions/MissionObjective.cs
@@ -4,10 +4,13 @@ namespace SurfaceRiseOfHumanity.Missions
 {
     public sealed class MissionObjective : MonoBehaviour
     {
-        [SerializeField] private string missionId = "MISSION_01_THE_SEALED_PATH";
+        [SerializeField] private string missionId = "M01_THE_SEALED_PATH";
+        [SerializeField] private string objectiveId = "M01_INVESTIGATE_SEALED_PATH";
         [SerializeField] private string objectiveText = "Investigate the sealed path";
         [SerializeField] private bool completeOnInteraction;
+
         public string MissionId => missionId;
+        public string ObjectiveId => objectiveId;
         public string ObjectiveText => objectiveText;
         public bool IsComplete { get; private set; }
 
@@ -17,5 +20,6 @@ namespace SurfaceRiseOfHumanity.Missions
         }
 
         public void Complete() => IsComplete = true;
+        public void RestoreCompleted(bool completed) => IsComplete = completed;
     }
 }
